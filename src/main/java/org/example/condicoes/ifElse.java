5e445809f77a8732cb5456d0347ade5fe28511f6
Eduardo Silva <thiago.novak7@gmail.com>
2025-06-26 09:46:38 -0300

    condicoes

---
+++ src/main/java/org/example/condicoes/ifElse.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Locale;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Naruto Uzumaki";
+        int idade = 16;
+        boolean hokage = false;
+        short numeroDeMissoes = 14;
+
+        if(numeroDeMissoes > 10) {
+            System.out.println("Naruto está pronto para passar de nível");
+        } else {
+            System.out.println("Naruto não pode passar de nível");
+        } 
+    
+    }    
+}
