5276473720154771e74f9ca6f07cf2d1f4cf0438
Eduardo Silva <thiago.novak7@gmail.com>
2025-06-27 12:06:48 -0300

    desafio01

---
+++ src/main/java/org/example/Desafio01/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Locale;
+
+
+public class Main {
+    public static void main(String[] args) {
+       String nomeNinja1 = "Naruto Uzumaki";
+       String missao = "Encontrar o bandido";
+       String statusDaMissao = "Em andamento";
+       char nivelDaMissao = 'A'; 
+
+       System.out.println("Nome do ninja: " + nomeNinja1)
+
+    }   
+}
