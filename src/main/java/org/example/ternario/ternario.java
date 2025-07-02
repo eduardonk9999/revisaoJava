0ab9e8019d9cb58c1bfdad8d47617534fcf05be8
Eduardo Silva <thiago.novak7@gmail.com>
2025-07-02 17:15:12 -0300

    ternario

---
+++ src/main/java/org/example/ternario/ternario.java
@@ -0,0 +1,10 @@
+
+
+public class Main() {
+    public void static Main(String[] args) {
+        short numeroDeMissoes = 11;
+
+        String nivel = (numeroDeMissoes >= 10) ? "Esse ninja pode" : "Esse ninja não pode";
+
+    }
+}
